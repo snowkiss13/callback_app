79ca4e91b8b7a335b744d496257cf6a44f6b9e3c
snowkiss13 <snowkiss13@gmail.com>
2022-04-03 17:19:41 +0900

    name値を補完するプライベートメソッドを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
+
+  private
+
+  def ensure_has_name
+    puts "nameの値をTaroに設定します！"
+    self.name = 'Taro' if name.blank?
+  end
 end
